0badcbf39cb32a52b64737bd4182f776164ee31d
Arthur <gorbenkoartur.ur@gmail.com>
2019-11-15 22:42:16 +0200

    Updates

---
+++ precache-manifest.1e08458275a0f6aa800611eba9efa3a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0f2f2e871201b4e7022fc55a9c4e5f",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "10e03309e1e59d96e4ce",
+    "url": "/NewsApp/static/css/main.c2ba2dff.chunk.css"
+  },
+  {
+    "revision": "0146ccb27b5ffb05626d",
+    "url": "/NewsApp/static/js/2.cbc47dd8.chunk.js"
+  },
+  {
+    "revision": "10e03309e1e59d96e4ce",
+    "url": "/NewsApp/static/js/main.645a8059.chunk.js"
+  },
+  {
+    "revision": "0822bccf044b9270124c",
+    "url": "/NewsApp/static/js/runtime-main.81efed51.js"
+  }
+]);
